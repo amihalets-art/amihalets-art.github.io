386a407eae264f96a02f756c637e6a0c4e8bac63
Amikhalets <amihalets@gmailcom>
2026-07-16 16:21:28 +0300

    Version 1

---
+++ JS-task1/Bubble Sort.js
@@ -0,0 +1,23 @@
+function bubbleSort(arr) {
+  let n = arr.length;
+  
+  for (let i = 0; i < n - 1; i++) {
+    let swapped = false; 
+    
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) { 
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        
+        swapped = true; 
+      }
+    }
+    
+    if (!swapped) break;
+  }
+  
+  return arr;
+}
+
+console.log("Bubble Sort:", bubbleSort([64, 34, 25, 12, 22, 11, 90]));
---
+++ JS-task1/Insertion Sort.js
@@ -0,0 +1,19 @@
+function insertionSort(arr) {
+  let n = arr.length;
+  
+  for (let i = 1; i < n; i++) {
+    let current = arr[i]; 
+    let j = i - 1;
+    
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j]; 
+      j--;
+    }
+    
+    arr[j + 1] = current;
+  }
+  
+  return arr;
+}
+
+console.log("Insertion Sort:", insertionSort([12, 11, 13, 5, 6]));
